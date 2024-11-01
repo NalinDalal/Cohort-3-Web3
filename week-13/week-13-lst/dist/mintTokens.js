77b33c82e403e88d55b75c03b2338e0637d28d04
Nalin <nalindalal2004@gmail.com>
2024-11-01 23:16:15 +0530

    removed secret api-key

--- week-13/week-13-lst/dist/mintTokens.js
+++ week-13/week-13-lst/dist/mintTokens.js
@@ -1,14 +1,14 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
+var __awaiter = (this && this.__awaiter) || function(thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function(resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function(resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
         function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
+var __importDefault = (this && this.__importDefault) || function(mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
@@ -18,7 +18,6 @@ const web3_js_1 = require("@solana/web3.js");
 const address_1 = require("./address");
 const bs58_1 = __importDefault(require("bs58"));
 // mint more tokens?
-//const connection = new Connection("https://mainnet.helius-rpc.com/?api-key=ec2b6ab3-3887-4a0a-9dff-fe83a3817fef");
 const connection = new web3_js_1.Connection("https://api.devnet.solana.com", "confirmed");
 function base58ToKeypair(base58PrivateKey) {
     try {
@@ -33,15 +32,15 @@ function base58ToKeypair(base58PrivateKey) {
 console.log("PRIVATE_KEY!");
 console.log(address_1.PRIVATE_KEY);
 const keypair = base58ToKeypair(address_1.PRIVATE_KEY);
-const mintTokens = (fromAddress, amount) => __awaiter(void 0, void 0, void 0, function* () {
+const mintTokens = (fromAddress, amount) => __awaiter(void 0, void 0, void 0, function*() {
     yield (0, spl_token_1.mintTo)(connection, keypair, address_1.TOKEN_MINT_ADDRESS, new web3_js_1.PublicKey(fromAddress), keypair, amount);
 });
 exports.mintTokens = mintTokens;
-const burnTokens = (fromAddress, toAddress, amount) => __awaiter(void 0, void 0, void 0, function* () {
+const burnTokens = (fromAddress, toAddress, amount) => __awaiter(void 0, void 0, void 0, function*() {
     console.log("Burning tokens");
 });
 exports.burnTokens = burnTokens;
-const sendNativeTokens = (fromAddress, toAddress, amount) => __awaiter(void 0, void 0, void 0, function* () {
+const sendNativeTokens = (fromAddress, toAddress, amount) => __awaiter(void 0, void 0, void 0, function*() {
     console.log("Sending native tokens");
 });
 exports.sendNativeTokens = sendNativeTokens;
